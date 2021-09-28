677da689ce94186d9921680e62f1d23a1f3f8dfc
Lloyd S Turley <lloydturley2@gmail.com>
2021-09-28 13:16:45 -0400

    implemented example web-based auth and calls for basic Google Calendar interaction.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,10 @@
-﻿using GoogleCalendarApiWebExample.Models;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.AspNetCore3;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using GoogleCalendarApiWebExample.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -9,6 +15,7 @@ using System.Threading.Tasks;
 
 namespace GoogleCalendarApiWebExample.Controllers
 {
+    [GoogleScopedAuthorize(CalendarService.ScopeConstants.CalendarEventsReadonly)]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -18,8 +25,99 @@ namespace GoogleCalendarApiWebExample.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index([FromServices] IGoogleAuthProvider auth)
         {
+            GoogleCredential cred = await auth.GetCredentialAsync();
+
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = cred,
+                ApplicationName = "Google Calendar Api Web Example",
+            });
+
+            List<calItem> familyEvents = new List<calItem>();
+
+            List<string> cals = new List<string>()
+            {
+                "3ebumnacuflq2qoljg18rbavn8@group.calendar.google.com",
+                "en.usa#holiday@group.v.calendar.google.com",
+                "lloydturley2@gmail.com",
+                "family14932423850734630239@group.calendar.google.com",
+                "vkroqg925v3afevfe5t62aq3i8@group.calendar.google.com",
+                "en.christian#holiday@group.v.calendar.google.com",
+                "f2qeksj4vjbvvscqilpe0a5hn4e3h9ij@import.calendar.google.com",
+                "waldentennessee@gmail.com",
+                "s93rp3t2qo3k16kot7bd4nqs2s@group.calendar.google.com"
+            };
+
+            foreach (var cal in cals)
+            {
+                EventsResource.ListRequest request = service.Events.List(cal);
+                request.TimeMin = DateTime.Now;
+                request.TimeMax = DateTime.Now.AddDays(7);
+                request.ShowDeleted = false;
+                request.SingleEvents = true;
+                //request.MaxResults = 10;
+                request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+                // List events.
+                Events events = request.Execute();
+                //Console.WriteLine("Summary: {0}", events.Summary);
+
+                if (events.Items != null && events.Items.Count > 0)
+                {
+                    //Console.WriteLine("Access role: {0}", events.AccessRole);
+                    //Console.WriteLine("Description: {0}", events.Description);
+                    //Console.WriteLine("ETage: {0}", events.ETag);
+                    //Console.WriteLine("Kind: {0}", events.Kind);
+                    //Console.WriteLine("Updated: {0}", events.Updated);
+                    //Console.WriteLine("Upcoming events:");
+
+                    bool allDay = false;
+
+                    foreach (var eventItem in events.Items)
+                    {
+                        DateTime when;
+                        if (eventItem.Start.DateTime == null)
+                        {
+                            when = DateTime.Parse(eventItem.Start.Date);
+                            allDay = true;
+                        }
+                        else
+                        {
+                            when = (DateTime)eventItem.Start.DateTime;
+                            allDay = false;
+                        }
+
+                        calItem fe = new calItem
+                        {
+                            CalName = events.Summary,
+                            EvtDate = when,
+                            EvtName = eventItem.Summary,
+                            AllDay = allDay
+                        };
+
+                        if (allDay == true)
+                        {
+                            string thing = $"All Day - {events.Summary} - {eventItem.Summary} - {when}";
+                        }
+                        else
+                        {
+                            string thing = $"{events.Summary} - {eventItem.Summary} - {when}";
+                        }
+
+                        familyEvents.Add(fe);
+
+                        //Console.WriteLine("{0} ({1})", eventItem.Summary, when);
+                    }
+                }
+                else
+                {
+                    //Console.WriteLine("No upcoming events found.");
+                }
+                //Console.WriteLine("----------------------------");
+            }
+
             return View();
         }
 
@@ -33,5 +131,13 @@ namespace GoogleCalendarApiWebExample.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+    }
+    public class calItem
+    {
+        public string CalName { get; set; }
+        public string EvtName { get; set; }
+        public DateTime EvtDate { get; set; }
+
+        public bool AllDay { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -4,15 +4,22 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Google.Apis.Auth.AspNetCore3;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace GoogleCalendarApiWebExample
 {
     public class Startup
     {
+
+        private const string ClientSecretFilenameVariable = "GoogleApiJson";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -24,6 +31,47 @@ namespace GoogleCalendarApiWebExample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            string clientSecretFilename = Environment.GetEnvironmentVariable(ClientSecretFilenameVariable);
+            if (string.IsNullOrEmpty(clientSecretFilename))
+            {
+                throw new InvalidOperationException($"Please set the {ClientSecretFilenameVariable} environment variable before running tests.");
+            }
+
+            var secrets = JObject.Parse(File.ReadAllText(clientSecretFilename))["web"];
+
+            // Check that this is a "web" client ID, not any other type of client ID like "installed app".
+            // The "web" element should have been present in the json so secrets value shouldn't be null.
+            if (secrets is null)
+            {
+                throw new InvalidOperationException(
+                    $"The type of the OAuth2 client ID specified in {ClientSecretFilenameVariable} should be Web Application. You can read more about setting up OAuth2 client IDs here: https://support.google.com/cloud/answer/6158849?hl=en");
+            }
+            var projectId = secrets["project_id"].Value<string>();
+            var clientId = secrets["client_id"].Value<string>();
+            var clientSecret = secrets["client_secret"].Value<string>();    
+
+
+            // This configures Google.Apis.Auth.AspNetCore3 for use in this app.
+            services
+                .AddAuthentication(o =>
+                {
+            // This forces challenge results to be handled by Google OpenID Handler, so there's no
+            // need to add an AccountController that emits challenges for Login.
+            o.DefaultChallengeScheme = GoogleOpenIdConnectDefaults.AuthenticationScheme;
+            // This forces forbid results to be handled by Google OpenID Handler, which checks if
+            // extra scopes are required and does automatic incremental auth.
+            o.DefaultForbidScheme = GoogleOpenIdConnectDefaults.AuthenticationScheme;
+            // Default scheme that will handle everything else.
+            // Once a user is authenticated, the OAuth2 token info is stored in cookies.
+            o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie()
+                .AddGoogleOpenIdConnect(options =>
+                {
+                    options.ClientId = clientId;
+                    options.ClientSecret = clientSecret;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +92,7 @@ namespace GoogleCalendarApiWebExample
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
